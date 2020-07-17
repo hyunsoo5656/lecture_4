fc266d1eaf16ee8ebd1924bfe8e1e1bf8aa4c28f
name <hyunsoo5656@gmail.com>
2020-07-17 10:38:05 +0900

    test

---
+++ is_test.py
@@ -0,0 +1,38 @@
+list_a = [10,20,30]
+
+list_b = [10,20,30]
+
+
+
+num_a ={"a":1, "b":1}
+num_b ={"a":1, "b":1}
+
+
+if num_a is num_b:
+    print('list_a is list_b')
+
+else:
+    print('list_a is not list_b')
+
+
+
+# if list_a == list_b:
+#     print('list_a is list_b')
+
+# else:
+#     print('list_a is not list_b')
+
+
+
+
+# print('list_A 는 {}'.format(id(list_a)))
+# print('list_B 는 {}'.format(id(list_b)))
+
+
+
+
+
+
+
+
+
